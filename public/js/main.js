defb9458de4761d2efd05765a2d05ff35c23121c
Scott <scottmtraver@gmail.com>
2017-05-21 00:39:43 -0600

    Add ticker for amount of fraud

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,27 @@
+// Created by Scott Traver
+var fraudAmount = new Date().getTime();// a really big growing number that won't repeat
+var fraudNumber = Number(fraudAmount.toString().substring(6));
+var formatFraud = function (input) {
+    return '$' + input.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+var tick = function () {
+    fraudNumber++;
+    document.getElementById('fraud-amount').innerHTML = formatFraud(fraudNumber)
+}
 
+var startValue = fraudNumber - 200;// animate to the current value a little bit
+var animate = function (element, endValue) {
+    var interval = setInterval(function () {
+        element.innerHTML = formatFraud(startValue);
+        startValue++;
+        if (startValue > endValue) {
+            clearInterval(interval)
+            // start ticking upward in seconds
+            setInterval(function () {
+                tick()
+            }, 1000);// tick interval in milliseconds
+        }
+    }, 1);// how fast it spinns up to the current value
+}
+
+animate(document.getElementById('fraud-amount'), fraudNumber);
